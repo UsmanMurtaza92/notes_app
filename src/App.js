a8440d80aacde70b4d9ee05f69321b9240356382
UsmanMurtaza92 <murtazausman041@gmail.com>
2023-08-22 11:44:38 +0500

    setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main className='text-center p-6 bg-gray-100'>Main Section</main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='text-center p-6'>Footer Component</div>
+  )
+}
+
+export default Footer
